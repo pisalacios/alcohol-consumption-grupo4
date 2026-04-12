59e0a7b7b67a0def1bca1c026e45e5905b630552
isabella <pisalacios2012@gmail.com>
2026-04-12 05:43:36 -0400

    Logo para la página + Filtro de busqueda + 1er valor interactivo

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-# Bliblioteca de paquetes
+    # Bliblioteca de paquetes y BBDD
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# Configuración de la página
+alcohol = pd.read_excel("alcohol_filtrado.xlsx")
+
+    # Configuración de la página
 
 st.set_page_config(
     page_title="Alcolismo Europa Dashboard Interactivo", 
     layout="wide",
+    page_icon="Logo de la Web.png"
 )
+
+    # Titulo, subtitulo y descripción
 
 st.write("## Análisis del Consumo de Alcohol en Europa 🍷")
 st.markdown("""
@@ -20,6 +25,20 @@ st.markdown("""
 En este dashboard interactivo podrás visualizar como fué el consumo anual de litros de 
 licor en diferentes paises de Europa por persona habitada
 """)
+    # Filtro de busqueda
 
-# Carga de la BBDD
-df = pd.read_excel("alcohol_filtrado.xlsx")
+st.sidebar.header("Opciones de Visualización")
+
+paises_disponibles = sorted(alcohol['pais'].unique())
+paises_seleccionados = st.sidebar.multiselect(
+    "Selecciona los países:",
+    options=paises_disponibles,
+    default=paises_disponibles
+)
+df_filtrado = alcohol[alcohol['pais'].isin(paises_seleccionados)]
+
+    # 1er cuadro de información
+
+sumatoria_total = df_filtrado['litros_por_persona'].sum()
+st.metric("Suma de litros p/p consumidos", f"{sumatoria_total:.2f}")
+st.caption("Sumatoria de litros consumidos en la seleción de la bsuqueda")
